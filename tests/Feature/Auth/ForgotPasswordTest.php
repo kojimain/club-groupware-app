6d980e91e9d853546bac3f0eb409a66c2d2a93db
kojimain <kojimainjp@gmail.com>
2019-02-21 00:15:57 +0900

    #5 各ページが200で表示されることのテスト

---
+++ tests/Feature/Auth/ForgotPasswordTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ForgotPasswordTest extends TestCase
+{
+    /**
+     * ページが表示されること
+     *
+     * @return void
+     */
+    public function test_ページが表示される()
+    {
+        $response = $this->get('/forget');
+
+        $response->assertStatus(200);
+    }
+}
