0b90a301a090749a4b9d30a23b8b697e27ebf33c
kojimain <kojimainjp@gmail.com>
2019-02-22 15:18:39 +0900

    #6 新規登録

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,58 +2,98 @@
 
 @section('content')
 <main>
-  <section class="hero is-light is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="column is-4 is-offset-4">
-          <img src="/img/logo.png" width="96" height="96" />
-          <div class="box">
-            <form action="/login">
-              <div class="field">
-                <div class="control">
-                  <label class="label">メールアドレス</label>
-                  <input
-                    class="input"
-                    type="email"
-                    placeholder="Email"
-                    autofocus
-                  />
+    <section class="hero is-light is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <img src="/img/logo.png" width="96" height="96" />
+                    <div class="box">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label">{{ __('validation.attributes.name') }}</label>
+                                    <input
+                                    class="input"
+                                    type="text"
+                                    name="name"
+                                    value="{{ old('name') }}"
+                                    placeholder="Name"
+                                    autofocus
+                                    required
+                                    />
+                                </div>
+                                @if ($errors->has('name'))
+                                <p class="help is-danger">
+                                    {{ $errors->first('name') }}
+                                </p>
+                                @endif
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label">{{ __('validation.attributes.email') }}</label>
+                                    <input
+                                    class="input"
+                                    type="email"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    placeholder="Email"
+                                    required
+                                    />
+                                </div>
+                                @if ($errors->has('email'))
+                                <p class="help is-danger">
+                                    {{ $errors->first('email') }}
+                                </p>
+                                @endif
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label">{{ __('validation.attributes.password') }}</label>
+                                    <input
+                                    class="input"
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    required
+                                    />
+                                </div>
+                                @if ($errors->has('password'))
+                                <p class="help is-danger">
+                                    {{ $errors->first('password') }}
+                                </p>
+                                @endif
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label">{{ __('validation.attributes.password') }}(確認)</label>
+                                    <input
+                                    class="input"
+                                    type="password"
+                                    name="password_confirmation"
+                                    placeholder="Password"
+                                    />
+                                </div>
+                                @if ($errors->has('password_confirmation'))
+                                <p class="help is-danger">
+                                    {{ $errors->first('password_confirmation') }}
+                                </p>
+                                @endif
+                            </div>
+                            <button
+                            class="button is-block is-primary is-large is-fullwidth"
+                            >
+                            登録
+                        </button>
+                    </form>
                 </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <label class="label">パスワード</label>
-                  <input
-                    class="input"
-                    type="password"
-                    placeholder="Password"
-                  />
-                </div>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <label class="label">パスワード(確認)</label>
-                  <input
-                    class="input"
-                    type="password"
-                    placeholder="Password"
-                  />
-                </div>
-              </div>
-              <button
-                class="button is-block is-primary is-large is-fullwidth"
-              >
-                登録
-              </button>
-            </form>
-          </div>
-          <p class="has-text-grey">
-            <a href="/login">ログインページに戻る</a>
-          </p>
+                <p class="has-text-grey">
+                    <a href="/login">ログインページに戻る</a>
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 </main>
 
 <footer class="footer">
--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -5,18 +5,77 @@ namespace Tests\Feature\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class RegisterTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * ページが表示されること
+     * GET /register ログインしていない場合
+     *
+     * App\Http\Controllers\Auth\RegisterController@showRegistrationForm
+     * ログインしていない場合、200で表示される
      *
      * @return void
      */
-    public function test_ページが表示される()
+    public function test_showRegistrationForm_whenLoggedOut()
     {
         $response = $this->get('/register');
 
+        # 200返す
         $response->assertStatus(200);
+    }
+
+    /**
+     * GET /register ログイン済みの場合
+     *
+     * App\Http\Controllers\Auth\RegisterController@showRegistrationForm
+     * ログイン済みの場合、 / へリダイレクトする
+     *
+     * @return void
+     */
+    public function test_showRegistrationForm_whenLoggedIn()
+    {
+        // ログインする
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        // アクセス
+        $response = $this->get('/register');
+
+        # / へリダイレクトする
+        $response->assertRedirect('/');
+    }
+
+    /**
+     * POST /register
+     *
+     * App\Http\Controllers\Auth\RegisterController@register
+     *
+     * @return void
+     */
+    public function test_register() {
+        $response = $this->post('/register', [
+            'name' => 'nametest',
+            'email' => 'test@example.com',
+            'password' => 'p4ssw0rd',
+            'password_confirmation' => 'p4ssw0rd'
+        ]);
+
+        # / へリダイレクト
+        $response->assertRedirect('/');
+
+        # 認証されている
+        $this->assertAuthenticated();
+
+        # Userが登録されている
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.com'
+        ]);
+
+        $user = User::first();
+        # api_tokenがセットされている
+        $this->assertNotNull($user->api_token);
     }
 }
