2a7bd45ef161ab77973f99053bdecac289dcba3f
kojimain <kojimainjp@gmail.com>
2019-02-23 21:27:41 +0900

    #7 パスワード再発行機能

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <main>
+        <section class="hero is-light is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <div class="column is-4 is-offset-4">
+                        <img src="/img/logo.png" width="96" height="96" />
+                        <div class="box">
+                            <form method="post" action="{{ route('password.update') }}">
+                                @csrf
+                                <input type="hidden" name="token" value="{{ $token }}">
+                                <div class="field">
+                                    <p>
+                                        パスワード再発行
+                                    </p>
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <label class="label">{{ __('validation.attributes.email') }}</label>
+                                        <input
+                                        class="input"
+                                        name="email"
+                                        type="email"
+                                        value="{{ old('email') }}"
+                                        placeholder="Email"
+                                        required
+                                        autofocus
+                                        />
+                                    </div>
+                                    @if ($errors->has('email'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('email') }}
+                                    </p>
+                                    @endif
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <label class="label">{{ __('validation.attributes.password') }}</label>
+                                        <input
+                                        class="input"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        required
+                                        />
+                                    </div>
+                                    @if ($errors->has('password'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('password') }}
+                                    </p>
+                                    @endif
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <label class="label">{{ __('validation.attributes.password') }}(確認)</label>
+                                        <input
+                                        class="input"
+                                        type="password"
+                                        name="password_confirmation"
+                                        placeholder="Password"
+                                        />
+                                    </div>
+                                    @if ($errors->has('password_confirmation'))
+                                    <p class="help is-danger">
+                                        {{ $errors->first('password_confirmation') }}
+                                    </p>
+                                    @endif
+                                </div>
+                                <button
+                                class="button is-block is-primary is-large is-fullwidth"
+                                >
+                                パスワード再発行
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+    </section>
+</main>
+
+<footer class="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+            <p>
+                (c) 2019 ClubGroupware(仮)
+            </p>
+        </div>
     </div>
-</div>
+</footer>
 @endsection
--- tests/Feature/Auth/ResetPasswordTest.php
+++ tests/Feature/Auth/ResetPasswordTest.php
@@ -5,18 +5,53 @@ namespace Tests\Feature\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Password;
+use App\User;
 
 class ResetPasswordTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * ページが表示されること
+     * GET /password/reset/{token}
+     *
+     * App\Http\Controllers\Auth\ResetPasswordController@showResetForm
      *
      * @return void
      */
-    public function test_ページが表示される()
+    public function test_showResetForm()
     {
-        $response = $this->get('/password/reset');
+        $user = factory(User::class)->create();
+        $token = Password::broker()->createToken($user);
 
+        // アクセス
+        $response = $this->get('/password/reset/'.$token);
+
+        // 200返す
         $response->assertStatus(200);
+    }
+
+    /**
+     * POST /password/reset
+     *
+     * App\Http\Controllers\Auth\ResetPasswordController@reset
+     *
+     * @return void
+     */
+    public function test_reset()
+    {
+        $user = factory(User::class)->create();
+        $token = Password::broker()->createToken($user);
+
+        // POSTする
+        $response = $this->post('/password/reset', [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => 'newpassword',
+            'password_confirmation' => 'newpassword'
+        ]);
+
+        // 200返す
+        $response->assertRedirect('/');
     }
 }
