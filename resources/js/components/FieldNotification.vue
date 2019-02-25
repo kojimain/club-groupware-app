9c3b3efe5ff72e534f1435dcd1b0ff4a0d8188a8
kojimain <kojimainjp@gmail.com>
2019-02-25 23:28:43 +0900

    #25 プロフィール編集

---
+++ resources/js/components/FieldNotification.vue
@@ -0,0 +1,16 @@
+<template>
+  <p
+    class="help"
+    :class="[`is-${type}`]">
+      {{ text }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: [
+    "type",
+    "text"
+  ]
+}
+</script>
---
+++ resources/js/components/Notification.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="notification"
+    :class="[`is-${type}`]">
+      {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "type",
+    "text"
+  ]
+}
+</script>
